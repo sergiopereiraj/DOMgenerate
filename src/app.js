e3fa21ceb8cab56c6dc7c21670ed95a1ec355923
sergiopereiraj <sergiopereira.jorquera@gmail.com>
2021-10-26 14:27:31 +0000

    guardado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function Dom() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  var pronoun = ["the", "our", "any", "kill", "yellow"];
+  var adj = ["great", "big", "nice", "tender", "tired"];
+  var noun = ["jogger", "racoon", "history", "way", "family"];
+  var dom = [".com", ".cl", ".co", ".mx", ".ae"];
+
+  for (var prono of pronoun) {
+    for (var adjetivo of adj) {
+      for (var non of noun) {
+        for (var dominio of dom) {
+          console.log(prono + adjetivo + non + dominio);
+        }
+      }
+    }
+  }
 };
